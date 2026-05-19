c1c8d91cc8c7af7c70d98640252f5c3eed418929
Chamdid123 <muhammadchamdid@gmail.com>
2026-05-20 00:29:42 +0300

    good

--- scripts/temples.js
+++ scripts/temples.js
@@ -1,2 +1,10 @@
+const navi = document.querySelector('.navi')
+const hamburger = document.querySelector('#menu');
+
+hamburger.addEventListener('click', () =>{
+    navi.classList.toggle('open');
+    hamburger.classList.toggle('open');
+
+});
 document.querySelector("#currentyear").textContent = new Date().getFullYear();
 document.querySelector("#lastModified").textContent = document.lastModified;
